bad4c389424524183a4d366085c61b9f23f63aa8
Phantom1st <caelum.is.amazing@gmail.com>
2019-09-21 11:07:11 -0600

    Unity Controller

---
+++ Assets/_Project/Scripts/UnityCharacterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityCharacterController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    float verticalVelocity;
+
+    void FixedUpdate()
+    {
+        verticalVelocity += -9.81f * Time.fixedDeltaTime;
+    }
+}
