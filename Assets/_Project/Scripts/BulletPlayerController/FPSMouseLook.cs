f8ee42fbe6180bce2ab806a8612a7788ad9a373a
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-09-22 21:40:38 -0600

    Better Mouselook

--- Assets/_Project/Scripts/BulletPlayerController/FPSMouseLook.cs
+++ Assets/_Project/Scripts/BulletPlayerController/FPSMouseLook.cs
@@ -1,5 +1,4 @@
-﻿using BulletUnity;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,13 +8,15 @@ public class FPSMouseLook : MonoBehaviour
 
     private Vector2 rotation = new Vector2(0, 0);
     [SerializeField] private float rotationSpeed = 10;
-
+    [Range(30,112)]
+    [SerializeField] private float rotationClamp = 80;
 
     void Update()
     {
-        rotation.x += -Input.GetAxis("Mouse Y");
-        camVertRot.transform.eulerAngles = new Vector2(rotation.x * rotationSpeed, transform.eulerAngles.y);
-        rotation.y += Input.GetAxis("Mouse X");
-        transform.eulerAngles = new Vector2(0, rotation.y) * rotationSpeed;
+        rotation.x += -Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -rotationClamp, rotationClamp);
+        camVertRot.transform.eulerAngles = new Vector2(rotation.x , transform.eulerAngles.y);
+        rotation.y += Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.eulerAngles = new Vector2(0, rotation.y);
     }
 }
