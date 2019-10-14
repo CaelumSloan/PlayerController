b4597c9224cfcac966c367b1bb13d34423235905
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-10-14 10:56:10 -0600

    Velocity Visualization

---
+++ Assets/_Project/Scripts/Debug/VelocityVisual.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VelocityVisual : MonoBehaviour
+{
+    [SerializeField] NextPositionFinder playerLogic;
+    [SerializeField] Text velocityText;
+
+    private float lastSpeed = Mathf.NegativeInfinity;
+    private float currentSpeed = Mathf.NegativeInfinity;
+
+    void Update()
+    {
+        Vector3 copyOfVelocity = playerLogic.PlayerVelocity;
+        copyOfVelocity.y = 0;
+        currentSpeed = Mathf.RoundToInt(copyOfVelocity.magnitude*100);
+        velocityText.text = currentSpeed.ToString();
+
+        if (currentSpeed > lastSpeed) velocityText.color = Color.green;
+        else if (currentSpeed < lastSpeed) velocityText.color = Color.red;
+        else velocityText.color = Color.white;
+
+        lastSpeed = currentSpeed;
+    }
+}
