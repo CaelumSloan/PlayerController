c61314bea042db38f61a1ccb5c84dc6d8a211127
Phantom-1st <caelum.sloan1@ucalgary.ca>
2019-11-02 18:20:31 -0600

    rotate and fov

---
+++ Assets/_Project/Scripts/BestestController/SpeedFOV.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedFOV : MonoBehaviour
+{
+    [SerializeField] private new Camera camera;
+    [SerializeField] private float fovEffectStrength = 10;
+
+    private float startFov = 90;
+
+    private void Start()
+    {
+        startFov = camera.fieldOfView;
+    }
+
+    void Update()
+    {
+
+        var vel = PlayerRefrenceHolder.Instance.nextPositionFinder.PlayerVelocity;
+        vel.y = 0;
+        var speed= vel.magnitude;
+
+        var normAngle = 1-Vector3.Angle(camera.transform.forward, PlayerRefrenceHolder.Instance.nextPositionFinder.PlayerVelocity) / 180;
+        float t = Mathf.Clamp01(speed / (1.5f * PlayerRefrenceHolder.Instance.nextPositionFinder.MaxSpeed));
+        float fov = Mathf.Lerp(startFov, startFov + fovEffectStrength, t*normAngle);
+        camera.fieldOfView = fov;
+    }
+}
--- Assets/_Project/Scripts/BestestController/VelocityRotator.cs
+++ Assets/_Project/Scripts/BestestController/VelocityRotator.cs
@@ -2,19 +2,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class VelocityRotator : MonoBehaviour
 {
     [SerializeField] private NextPositionFinder player;
+    [SerializeField] private new Camera camera;
+
+    float acceleration = 0;
+    Queue<float> lastSpeeds = new Queue<float>(5);
 
     void Update()
     {
         if (player.PlayerVelocity.magnitude > Mathf.Epsilon)
         {
             var cross = Vector3.Cross(transform.InverseTransformDirection(player.PlayerVelocity.normalized), Vector3.up);
-            transform.localRotation = Quaternion.Slerp(Quaternion.identity, Quaternion.AngleAxis(-7.5f, cross), Mathf.Clamp01(player.PlayerVelocity.magnitude / (1.5f * player.MaxSpeed)));
+            var normAngle = 1 - Vector3.Angle(camera.transform.forward, PlayerRefrenceHolder.Instance.nextPositionFinder.PlayerVelocity) / 180;
+            transform.localRotation = Quaternion.Slerp(Quaternion.identity, Quaternion.AngleAxis(-7.5f, cross), Mathf.Clamp01(player.PlayerVelocity.magnitude / (1.5f * player.MaxSpeed)) * normAngle * Mathf.Clamp01(acceleration/350f));
         }
         else
             transform.localRotation = Quaternion.identity;
+    }
+
+    private void FixedUpdate()
+    {
+        acceleration = AccelerationFinder();
+    }
+
+    float i=0;
+    float AccelerationFinder()
+    {
+        float last=0;
+        if (i < 6) i++;
+        else
+            last=lastSpeeds.Dequeue();
+
+        var vel = player.PlayerVelocity;
+        vel.y = 0;
+        lastSpeeds.Enqueue(vel.magnitude);
+
+        return Mathf.Abs(last-vel.magnitude) / (Time.deltaTime * lastSpeeds.Count);
     }
 }
